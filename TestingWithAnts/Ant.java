6ef0615fe8373d34e6b8d6c02d45ca5fea748129
Kevin Tsui <kjtsui10@gmail.com>
2015-04-30 11:17:16 -0700

    meh

--- TestingWithAnts/Ant.java
+++ TestingWithAnts/Ant.java
@@ -8,7 +8,24 @@ import greenfoot.*;
  */
 public class Ant extends Enemy
 {
-    public static final int SPEED = 1;
+    // FINAL VARIABLES
+    public static final int SPEED = 1; // ant speed
+    public static final int TAKE_TIME = 30; // number of cycles (in timer) to take food
+    public static final int DROP_TIME = 180; // number of cycles to drop food
+    
+    // If Ant has food
+    public boolean hasFood;
+    
+    // Timers
+    public int takeTimer; // when ant is trying to take food
+    public int dropTimer; // when ant is dropping the food
+    
+    public Ant()
+    {
+        hasFood = false;
+        takeTimer = TAKE_TIME;
+        dropTimer = DROP_TIME;
+    }
     
     /**
      * Act - do whatever the Ant wants to do. This method is called whenever
@@ -25,6 +42,14 @@ public class Ant extends Enemy
      */
     private void walk()
     {
+        if (hasFood == false) // DOESN'T HAVE food
+        {
+            smellFood();
+        }
+        else // HAS food
+        {
+            headHome();
+        }
         randomWalk();
     }
     
@@ -45,6 +70,114 @@ public class Ant extends Enemy
         if (getY() <= 5 || getY() >= getWorld().getHeight() - 5)
         {
             turn(180);
+        }
+    }
+    
+    /**
+     * Turn towards the food
+     */
+    public void smellFood()
+    {
+        Food food = null;
+        
+        // Ant is on top of the food
+        if (getObjectsInRange(20, Food.class).isEmpty() == false)
+        {
+            if (Greenfoot.getRandomNumber(1000) < 500)
+            {
+                food = (Food)(getObjectsInRange(1700, Food.class).get(0));
+                turnTowards(food.getX(), food.getY());
+            }
+        }
+        // Ant is close to the food
+        else if (getObjectsInRange(100, Food.class).isEmpty() == false)
+        {
+            if (Greenfoot.getRandomNumber(1000) < 150)
+            {
+                food = (Food)(getObjectsInRange(1700, Food.class).get(0));
+                turnTowards(food.getX(), food.getY());
+            }
+        } 
+        // Ant is getting close to the food
+        else if (getObjectsInRange(200, Food.class).isEmpty() == false)
+        {
+            if (Greenfoot.getRandomNumber(1000) < 25)
+            {
+                food = (Food)(getObjectsInRange(200, Food.class).get(0));
+                turnTowards(food.getX(), food.getY());
+            }
+        }
+        // Ant is far from the food
+        else if (getWorld().getObjects(Food.class).isEmpty() == false)
+        {
+            if (Greenfoot.getRandomNumber(1000) < 5)
+            {
+                food = (Food)(getWorld().getObjects(Food.class).get(0));
+                turnTowards(food.getX(), food.getY());
+            }
+        }
+        
+        if (food != null && intersects(food))
+        {
+            takeFood(food);
+        }
+    }
+    
+    /**
+     * Take some food after some time has past
+     */
+    public void takeFood(Food food)
+    {
+        if (takeTimer == 0)
+        {
+            hasFood = true;
+            food.takeSome(Greenfoot.getRandomNumber(3) + 1);
+            takeTimer = TAKE_TIME;
+        }
+        else
+        {
+            takeTimer--;
+        }
+    }
+    
+    /**
+     * Head towards home
+     */
+    public void headHome()
+    {
+        AntHome home = null;
+        
+        // Just checking if there's a home
+        if (getWorld().getObjects(AntHome.class).isEmpty() == false)
+        {
+            home = (AntHome)(getWorld().getObjects(AntHome.class).get(0));
+            if (Greenfoot.getRandomNumber(1000) < 120)
+            {
+                turnTowards(home.getX(), home.getY());
+            }
+        }
+        
+        // check if ant has reached home
+        if (home != null && intersects(home))
+        {
+            dropFood();
+        }
+    }
+    
+    /**
+     * Ant drops the food after some time has past
+     */
+    public void dropFood()
+    {
+        if (dropTimer == 0)
+        {
+            hasFood = false;
+            /* setImage("...") or something */
+            dropTimer = DROP_TIME * 2;
+        }
+        else
+        {
+            dropTimer--;
         }
     }
     
--- TestingWithAnts/Food.java
+++ TestingWithAnts/Food.java
@@ -8,12 +8,61 @@ import greenfoot.*;
  */
 public class Food extends Actor
 {
+    public static final int FOOD_COUNT = 35;
+    
+    /* Instance Variables */
+    public int count;
+    public setting world;
+    
+    /**
+     * Constructor for Food
+     */
+    public Food()
+    {
+        world = (setting)getWorld();
+        count = FOOD_COUNT;
+    }
+    
     /**
      * Act - do whatever the Food wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        //update();
+    }
+    
+    /**
+     * Update this object
+     */
+    public void update()
+    {
+        if (count <= 0)
+        {
+            world.removeObject(this);
+        }
+    }
+    
+    /**
+     * Take some food
+     */
+    public void takeSome(int amount)
+    {
+        if (count - amount >= 0)
+        {
+            count -= amount;
+        }
+        else
+        {
+            count = 0;
+        }
+    }
+    
+    /**
+     * Simple 'get' method to get count
+     */
+    public int getCount()
+    {
+        return count;
+    }
 }
