ee3c600f5c55a9407c2b480bf45b6bcae920ebef
Brian Luu <beluu@calpoly.edu>
2015-05-08 13:28:49 -0700

    Spawner - Now moves around randomly on the top right of screen. - Can adjust delay and area that the spawner moves

--- TestingWithAnts/Spawner.java
+++ TestingWithAnts/Spawner.java
@@ -12,7 +12,22 @@ public class Spawner extends Actor
     public static int spawnRange = 1000;
     public static int fastAntThreshold = 40;
     public final static int maxAnts = 200;
+    
+    private final static int MIN_X = 600;
+    private final static int RANDOM_X = 200;
+    private final static int MIN_Y = 10;
+    private final static int RANDOM_Y = 100;
+    
+    private int delay;
+    private final static int DELAY_MOVEMENT = 50;
    
+    public Spawner() {
+        GreenfootImage image = new GreenfootImage("apple1.png");
+        setImage(image);
+        image.setTransparency(100);
+        delay = 0;
+    }
+    
     /**
      * Act - do whatever the Spawner wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -20,6 +35,13 @@ public class Spawner extends Actor
     public void act() 
     {
         setting w = (setting)getWorld();
+        
+        if (delay >= 50) {
+            setLocation(MIN_X + Greenfoot.getRandomNumber(RANDOM_X), 
+                        MIN_Y + Greenfoot.getRandomNumber(RANDOM_Y));
+            delay = 0;
+        }
+        delay++;
         
         if (w.getObjects(Enemy.class).size() < maxAnts) {
             switch (w.level) {
